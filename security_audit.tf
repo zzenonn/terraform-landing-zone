51dbe0ebb20f5c3bf47936ab8db7e0ba542b880b
Techdebt Remediation Agent <techdebt-agent@automated.local>
2026-03-29 22:39:06 +0800

    Fix Terraform provider deprecations in security_audit.tf and catalog_account.tf

--- security_audit.tf
+++ security_audit.tf
@@ -32,27 +32,6 @@ resource "aws_guardduty_organization_configuration" "guard_duty" {
 
   detector_id = aws_guardduty_detector.guard_duty_delegated_administrator.id
 
-  datasources {
-    # Expensive because of the volume of logs. Enable only if needed
-    s3_logs {
-      auto_enable = false
-    }
-    kubernetes {
-      audit_logs {
-        enable = true
-      }
-    }
-
-    # Expensive. Enable only if needed
-    malware_protection {
-      scan_ec2_instance_with_findings {
-        ebs_volumes {
-          auto_enable = false
-        }
-      }
-    }
-  }
-
   depends_on = [aws_guardduty_organization_admin_account.guard_duty_delegated_administrator]
 }
 
@@ -230,4 +209,25 @@ resource "aws_guardduty_publishing_destination" "centralized_logging" {
   depends_on = [
     aws_s3_bucket_policy.guard_duty_findings_logs_policy,
   ]
+}
+
+resource "aws_guardduty_organization_configuration_feature" "s3_logs" {
+  provider    = aws.security_account
+  detector_id = aws_guardduty_detector.guard_duty_delegated_administrator.id
+  name        = "S3_DATA_EVENTS"
+  auto_enable = "NONE"
+}
+
+resource "aws_guardduty_organization_configuration_feature" "eks_audit_logs" {
+  provider    = aws.security_account
+  detector_id = aws_guardduty_detector.guard_duty_delegated_administrator.id
+  name        = "EKS_AUDIT_LOGS"
+  auto_enable = "ALL"
+}
+
+resource "aws_guardduty_organization_configuration_feature" "ebs_malware_protection" {
+  provider    = aws.security_account
+  detector_id = aws_guardduty_detector.guard_duty_delegated_administrator.id
+  name        = "EBS_MALWARE_PROTECTION"
+  auto_enable = "NONE"
 }
